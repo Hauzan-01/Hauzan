782cd81553c98f6884ac162b2ce3f40f03ac792c
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 22:04:49 +0530

    Add files via upload

---
+++ SRI codes/Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("This is our first program to learn variables in C#.");
+            int x = 5;
+            int y = x + 7;
+            Console.WriteLine(y);
+            
+
+            string firstname, lastname;
+            Console.WriteLine("Let's see what your name is.");
+            Console.Write("Enter your first name: ");
+            firstname = Console.ReadLine();             // for scanning values
+            Console.Write("Enter your last name: ");
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("Hi, your name is " + firstname + " " + lastname + ".");
+            Console.ReadLine();
+        }
+    }
+}
+
