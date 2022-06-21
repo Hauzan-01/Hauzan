724b6ae7555d1f452a48169d75dbb3a85f6a78c3
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 21:59:33 +0530

    Add files via upload

---
+++ SRI codes/Handling Exceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For exceptions - try, catch, finally blocks
+            try
+            {
+                string content = File.ReadAllText(@"D:\Exampl.txt");
+                Console.WriteLine(content);
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem.");
+                Console.WriteLine("The specified file \"{0}\" was not found. Make sure you named it correctly.", "Exampl.txt");
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem.");
+                Console.WriteLine("The specified directory was not found. Make sure the file path is correct.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There was a problem.");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // code to finalize
+                // things like - set objects to null, close database connections, etc.
+                Console.WriteLine("Closing application now.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
