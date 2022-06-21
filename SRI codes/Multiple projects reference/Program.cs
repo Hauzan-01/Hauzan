f426e0b9f56f9b4bf599cd0acc07b23b119a77f4
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 22:01:20 +0530

    Add files via upload

---
+++ SRI codes/Multiple projects reference/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ScrapeLibrary;
+
+namespace myClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebPage("http://www.google.com");
+            Console.WriteLine(value);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SRI codes/Multiple projects reference/Scrape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebPage(url);
+        }
+
+        public string ScrapeWebPage(string url, string filepath)
+        {
+            string reply = GetWebPage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebPage(string url)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("https://google.com");
+            reply += " THAT'S ALL FOLKS....!!!";
+            return reply;
+        }
+    }
+}
