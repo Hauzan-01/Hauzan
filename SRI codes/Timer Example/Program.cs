4cefa079532224d31c46b6096ecc393ab5d4f7e4
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 22:03:23 +0530

    Add files via upload

---
+++ SRI codes/Timer Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace TimerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);            // 2000 milliseconds = 2 seconds
+
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            myTimer.Start();
+            
+            Console.WriteLine("Press ENTER to to remove red event.");
+            Console.ReadLine();
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss:fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss:fff}", e.SignalTime);
+        }
+
+    }
+}
