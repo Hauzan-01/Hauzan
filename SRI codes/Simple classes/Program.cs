e0ea9cc876dafc10385b6dbdb714aa63c524bf08
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 22:02:35 +0530

    Add files via upload

---
+++ SRI codes/Simple classes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cake c = new Cake();
+            c.name = "Choco-moco";
+            c.flavor = "Chocolate";
+            c.price = 34.50F;
+            c.weight = 3;
+
+            Console.WriteLine("Cake properties: ");
+            Console.WriteLine(c.name);
+            Console.WriteLine(c.flavor);
+            Console.WriteLine(c.price);
+            Console.WriteLine(c.weight);
+
+            Console.WriteLine("Final price: ${0}", c.getPrice());
+            Console.ReadLine();
+        }
+    }
+
+    class Cake
+    {
+        public string name { get; set; }
+        public string flavor { get; set; }
+        public float price { get; set; }
+        public float weight { get; set; }
+
+        public float getPrice()
+        {
+            if (weight > 2.5)
+                return price + 50;
+            else
+                return price; 
+        } 
+    }
+}
+
