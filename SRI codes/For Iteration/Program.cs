558b490536ed7f6012b53ad32ac1da28cbf3a075
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 21:59:14 +0530

    Add files via upload

---
+++ SRI codes/For Iteration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // just like regularly known 'for' loop
+
+            Console.WriteLine("Table of 26:");
+            for (int k = 1; k <= 10; k++)
+            {
+                Console.WriteLine("26 x {0} = {1}", k, 26*k);
+            }
+
+            Console.WriteLine("Adding numbers from 1 to 25 using a for loop:");
+            int sum = 0;
+            for(int k = 1; k<26; k++)
+            {
+                sum += k;
+            }
+            Console.WriteLine("The answer is: {0}", sum);
+
+            Console.ReadLine();
+        }
+    }
+}
