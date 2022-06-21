1d9b70a00b6f830f89d22883437190854f52d939
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 21:57:33 +0530

    Add files via upload

---
+++ SRI codes/Assemblies and Namespaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text =
+            "A class is the most powerful data type in C#. Like a structure, " +
+            "a class defines the data and behavior of the data type. ";
+
+            File.WriteAllText("WriteText.txt", text);
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("https://google.com");
+            File.WriteAllText("WriteText.txt", reply);
+            Console.WriteLine(reply);
+
+            Console.ReadLine();
+        }
+    }
+}
