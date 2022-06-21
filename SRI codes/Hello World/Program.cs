9577d647f0b50988c6056549a49b92aa023a762b
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 22:00:46 +0530

    Add files via upload

---
+++ SRI codes/Hello World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!. This is my first C# program.");
+            Console.WriteLine("It is fun to begin with C#.");
+            Console.ReadLine();     // this line helps to make the program wait to execute its further code
+        }
+    }
+}
+
+
