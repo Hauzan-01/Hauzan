287ff99ddd05fb33d7f571e04c2ee66507205e52
Hauzan-01 <101184067+Hauzan-01@users.noreply.github.com>
2022-06-21 22:04:26 +0530

    Add files via upload

---
+++ SRI codes/Understanding Scope/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UnderstandingScope
+{
+    class Program
+    {
+        private static int k;
+        static void Main(string[] args)
+        {
+            int j = 0;
+            for(int i=0; i<25; i++)
+            {
+                j = i;
+                k = i;
+                //Console.WriteLine(i);
+            }
+
+            //Console.WriteLine(i);
+            Console.WriteLine("j = " + j);
+            Console.WriteLine("k = " + k);
+
+            helperMethod();
+
+            // using class scope, 
+            Car myCar = new Car();
+            myCar.Introduce();
+
+            Console.ReadLine();
+        }
+
+        private static void helperMethod()
+        {
+            Console.WriteLine("k = " + k);
+        }
+    }
+
+    class Car
+    {
+        public void Introduce()
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod()
+        {
+            return "Hello, world!";
+        }
+    }
+}
